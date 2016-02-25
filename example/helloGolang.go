f735e8a15ccc9030b6985edae33f554e963ce0fc
jackhu <jackhu1990@163.com>
2016-02-25 18:42:37 +0800

    第二章补充

--- example/helloGolang.go
+++ example/helloGolang.go
@@ -26,6 +26,33 @@ func test(slice []string)(){
 	return
 }
 func main() {
+	fmt.Println("Hello Golang")
+
+	result,err := Add(0,10)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(result)
+
+	if i := 10; i < 5 {
+		fmt.Print("***********")
+	}
+	var person PersonBase
+	var personP *PersonBase
+	person.Name = "jackhu"
+	person.Age = 27
+	personP = new(PersonBase)
+	personP.Name = "gaofei"
+	personP.Age = 27
+
+	arr :=[...] int {1,2,3,4,5}
+	for i,v:= range arr{
+		fmt.Println("index:", i, "value:", v)
+	}
+	m := map[int]string{1:"one", 2:"two"}
+	for i,v:= range m{
+		fmt.Println("index:", i, "value:", v)
+	}
 	s := []string{"1","2","3"}
 	s1:= s;
 	for i,v:=range s {
